8ccd230075cbb5fb7a4b29a2c3a1e7d56504a664
DilshadIlyas0422 <dilshadilyas04@gmail.com>
2025-03-03 01:34:54 -0800

    Delete Chapter 01_Output, Input, Variables, Operators/PrintingNumbers.java

--- Chapter 01_Output, Input, Variables, Operators/PrintingNumbers.java
+++
@@ -1,8 +0,0 @@
-public class PrintingNumbers{
-    public static void main(String[] args) {
-      System.out.println("4");
-      System.out.println(4);
-      System.out.println(4+3);
-      System.out.println("4+3");
-    }
-  }
